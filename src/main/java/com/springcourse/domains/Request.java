c05d20be557474032bfc3dea7e8526148e6d3a7f
Lucas Lozano <lucasrlozanocm@gmail.com>
2019-06-14 20:28:53 -0300

    Map entities

--- src/main/java/com/springcourse/domains/Request.java
+++ src/main/java/com/springcourse/domains/Request.java
@@ -4,19 +4,55 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.springcourse.enums.RequestState;
+
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "request")
 public class Request {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;	
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name="creation_date")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date criationDate;
-	private RequestStage state;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
+	
+	@OneToMany(mappedBy = "request")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
--- src/main/java/com/springcourse/domains/User.java
+++ src/main/java/com/springcourse/domains/User.java
@@ -3,6 +3,15 @@ package com.springcourse.domains;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import com.springcourse.enums.Role;
 
 import lombok.*;
@@ -11,13 +20,30 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name="user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(length = 75, nullable = false)
 	private String name;
+	
+	@Column(length = 75, nullable = false)
 	private String email;
+	
+	@Column(length = 75, nullable = false)
 	private String password;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	@OneToMany(mappedBy = "user")
 	private List<Request> requests = new ArrayList<Request>();
+	
+	@OneToMany(mappedBy = "user")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
