2c47a9607529d5eec343f2c7d805e4be1d427e17
Lucas Lozano <lucasrlozanocm@gmail.com>
2019-06-13 23:37:20 -0300

    add role attribute in User class

--- src/main/java/com/springcourse/domains/User.java
+++ src/main/java/com/springcourse/domains/User.java
@@ -13,6 +13,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
