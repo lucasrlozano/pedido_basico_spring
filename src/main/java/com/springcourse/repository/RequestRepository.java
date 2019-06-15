ad9ad6ce954d88c36a9b050120f989306d442e34
Lucas Lozano <lucasrlozanocm@gmail.com>
2019-06-15 10:29:06 -0300

    Add updateStatus in RequestRepository

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springcourse.domains.Request;
+import com.springcourse.enums.RequestState;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
 	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
 }
