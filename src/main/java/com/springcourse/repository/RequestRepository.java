484b70c51313c04ec702bc4960a7323c02e4ca0d
Lucas Lozano <lucasrlozanocm@gmail.com>
2019-06-26 22:11:29 -0300

    Update RequestRepository.java

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -3,7 +3,10 @@ package com.springcourse.repository;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +18,8 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 
 	public List<Request> findAllByOwnerId(Long id);
 	
+	@Transactional
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
-	public Request updateStatus(Long id, RequestState state);
+	public void updateStatus(Long id, RequestState state);
 }
